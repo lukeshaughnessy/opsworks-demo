62d17f540b62ab90c12ec8df239e3613990805e7
Luke Shaughnessy <lshaughnessy@alteryx.com>
2015-12-31 12:10:25 -0700

    second

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -1,6 +1,5 @@
-
 file "/etc/motd" do
-content "Property of Luke
+content "Property of Luke the Awesome
 IPADDRESS: #{node["ipaddress"]}
 HOSTNAME: #{node["hostname"]} 
 MEMORYIS: #{node["memory"]["total"]}
